1456a65e8739fd3d056dd5b7165cbb2d9178d5e2
monkipon <154270946+monkipon@users.noreply.github.com>
2024-01-02 02:52:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import asyncio
 import random
 import art
+import time
 
 from web3go import Web3Go
 from utils import read_private_keys, read_proxies
 
-async def main():
 
-    art_text = art.text2art('Web3Go') 
-    lines = "-" * len(art_text.split('\n')[0])
-    print(f"{lines}\n{art_text}{lines}")
-    print('Создатель: https://t.me/Genjurx')
-
-    private_keys = await read_private_keys('keys.txt')
-    proxies = await read_proxies('proxies.txt')
-
-    for private_key in private_keys:
+async def process_private_key(private_key, proxies, semaphore):
+    async with semaphore:
         proxy = random.choice(proxies) if proxies else None
         async with Web3Go(private_key, proxy) as web3go_bot:
             result = await web3go_bot.claim()
-            proxy_ipport = web3go_bot.proxy.split('@')
-            print(f"{web3go_bot.address} | {result} | {proxy_ipport[1]}")
+            proxy_pass = web3go_bot.proxy.split('@')
+            print(f"{web3go_bot.address} | {result} | {proxy_pass[1]}")
+
+
+async def main():
+    art_text = art.text2art('Web3Go')
+    lines = "-" * len(art_text.split('\n')[0])
+    print(f"{lines}\n{art_text}{lines}")
+    print('Создатель: https://t.me/Genjurx')
+    time1 = time.time()
+    private_keys = await read_private_keys('keys.txt')
+    proxies = await read_proxies('proxies.txt')
+
+    semaphore = asyncio.Semaphore(5)
+
+    tasks = [process_private_key(private_key, proxies, semaphore) for private_key in private_keys]
+    await asyncio.gather(*tasks)
+
+    time2 = time.time()
+    final_time = time2 - time1
+    print(final_time)
 
 if __name__ == "__main__":
     asyncio.run(main())
