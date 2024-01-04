cb0861c71c1c826b6d86b451cfe75a033008b49c
monkipon <154270946+monkipon@users.noreply.github.com>
2024-01-04 15:40:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,34 @@ async def process_private_key(private_key, proxies, semaphore):
         proxy = random.choice(proxies) if proxies else None
         async with Web3Go(private_key, proxy) as web3go_bot:
             result = await web3go_bot.claim()
-            print(f"{web3go_bot.address} | {result} | {web3go_bot.proxy_ip}")
+            streak = await web3go_bot.get_streak_days()
+            print(f"{web3go_bot.address} | {result} | {streak} |{web3go_bot.proxy_ip}")
 
 
-async def main():
+def make_art():
     art_text = art.text2art('Web3Go')
     lines = "-" * len(art_text.split('\n')[0])
     print(f"{lines}\n{art_text}{lines}")
     print('Создатель: https://t.me/Genjurx')
+
+
+async def main():
+    make_art()
     time1 = time.time()
+
     private_keys = await read_private_keys('keys.txt')
     proxies = await read_proxies('proxies.txt')
 
-    semaphore = asyncio.Semaphore(5)
+    semaphore = asyncio.Semaphore(10)
 
     tasks = [process_private_key(private_key, proxies, semaphore) for private_key in private_keys]
+
     await asyncio.gather(*tasks)
 
     time2 = time.time()
     final_time = time2 - time1
     print(final_time)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
